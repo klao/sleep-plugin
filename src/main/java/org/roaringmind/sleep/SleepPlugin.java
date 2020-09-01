e6f7cc94a3f483b7143348518edcca2bbc04304d
Nandor Kovacs <nandor1kovacs@gmail.com>
2020-09-01 21:36:18 +0200

    FastWalking

--- src/main/java/org/roaringmind/sleep/SleepPlugin.java
+++ src/main/java/org/roaringmind/sleep/SleepPlugin.java
@@ -1,11 +1,14 @@
 package org.roaringmind.sleep;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.event.player.PlayerBedEnterEvent.BedEnterResult;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -39,7 +42,46 @@ public class SleepPlugin extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerBedEnter(PlayerBedEnterEvent event) {
-        shout("" + event.getPlayer().getName() + " entered bed");
+        if (event.getBedEnterResult() != BedEnterResult.OK) {
+            return;
+        }
+        shout("" + event.getPlayer().getName() + " wants to sleep");
+        for (var p : getServer().getOnlinePlayers()) {
+            shout("Online:" + p.getName());
+            
+            if (p == event.getPlayer()) {
+                continue;
+            
+
+            p.setGameMode(GameMode.SPECTATOR);
+            p.setWalkSpeed(0);
+            p.setFlySpeed(0);
+            // var bed = p.getBedSpawnLocation();
+            // shout(p.getName() + " " + bed);
+            // if (!p.sleep(p.getLocation(), true)) {
+            //     shout(p.getName() + " " + "not succesfull");
+            // }
+        }
+
     }
 
+    @EventHandler
+    public void onPlayerBedLeave(PlayerBedLeaveEvent event) {
+        for (var p : getServer().getOnlinePlayers()) {
+            
+            if (p == event.getPlayer()) {
+                continue;
+            }
+            
+
+            p.setGameMode(GameMode.SURVIVAL);
+            p.setWalkSpeed(1);
+            p.setFlySpeed(0);
+            // var bed = p.getBedSpawnLocation();
+            // shout(p.getName() + " " + bed);
+            // if (!p.sleep(p.getLocation(), true)) {
+            //     shout(p.getName() + " " + "not succesfull");
+            // }
+        }
+    }
 }
