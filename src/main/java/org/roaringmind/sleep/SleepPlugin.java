299d730dae499f4818b1c9291ba311744f71aeab
Mihaly Barasz <klao@nilcons.com>
2020-09-03 18:46:01 +0200

    Add structured message example

--- src/main/java/org/roaringmind/sleep/SleepPlugin.java
+++ src/main/java/org/roaringmind/sleep/SleepPlugin.java
@@ -7,12 +7,17 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
-import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.event.player.PlayerBedEnterEvent.BedEnterResult;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-// TODO: "borrow" features from:
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ClickEvent.Action;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+
+// We can "borrow" features from:
 // https://github.com/Nuytemans-Dieter/BetterSleeping
 // https://github.com/JoelGodOfwar/SinglePlayerSleep
 
@@ -40,52 +45,42 @@ public class SleepPlugin extends JavaPlugin implements Listener {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        shout("" + sender + " sent " + command.getName());
-        return super.onCommand(sender, command, label, args);
+        shout("" + sender + " sent " + command.getName() + " " + String.join(" ", args));
+        return true;
     }
 
     @EventHandler
     public void onPlayerBedEnter(PlayerBedEnterEvent event) {
-        if (event.getBedEnterResult() != BedEnterResult.OK) {
-            return;
-        }
+        if (event.getBedEnterResult() != BedEnterResult.OK) return;
+
+        var msg = new ComponentBuilder("OK to sleep?  ")
+            .append("[Yes]").color(ChatColor.DARK_GREEN).bold(true).event(new ClickEvent(Action.RUN_COMMAND, "/sleepy yes"))
+            .append("  ")
+            .append("[No]").color(ChatColor.DARK_RED).bold(true).event(new ClickEvent(Action.RUN_COMMAND, "/sleepy no"))
+            .create();
+
+        // Lehetne egyszerre mindenkinek kuldeni igy:
+        // getServer().spigot().broadcast(msg);
+
         shout("" + event.getPlayer().getName() + " wants to sleep");
         for (var p : getServer().getOnlinePlayers()) {
-            shout("Online:" + p.getName());
-            
-            if (p == event.getPlayer()) {
-                continue;
-            
+            p.spigot().sendMessage(msg);
+            if (p == event.getPlayer()) continue;
 
             p.setGameMode(GameMode.SPECTATOR);
             p.setWalkSpeed(0);
             p.setFlySpeed(0);
-            // var bed = p.getBedSpawnLocation();
-            // shout(p.getName() + " " + bed);
-            // if (!p.sleep(p.getLocation(), true)) {
-            //     shout(p.getName() + " " + "not succesfull");
-            // }
         }
-
     }
 
     @EventHandler
     public void onPlayerBedLeave(PlayerBedLeaveEvent event) {
         for (var p : getServer().getOnlinePlayers()) {
-            
-            if (p == event.getPlayer()) {
-                continue;
-            }
-            
+            if (p == event.getPlayer()) continue;
 
             p.setGameMode(GameMode.SURVIVAL);
             p.setWalkSpeed(1);
             p.setFlySpeed(0);
-            // var bed = p.getBedSpawnLocation();
-            // shout(p.getName() + " " + bed);
-            // if (!p.sleep(p.getLocation(), true)) {
-            //     shout(p.getName() + " " + "not succesfull");
-            // }
         }
     }
 }
