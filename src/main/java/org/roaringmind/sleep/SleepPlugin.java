5fbdf89ed3e22b455659f64eaef08dd5b15ecd8f
Nandor Kovacs <nandor1kovacs@gmail.com>
2020-09-03 21:45:21 +0200

    sleepy yes shouts

--- src/main/java/org/roaringmind/sleep/SleepPlugin.java
+++ src/main/java/org/roaringmind/sleep/SleepPlugin.java
@@ -1,9 +1,14 @@
 package org.roaringmind.sleep;
 
+import java.util.HashMap;
+import java.util.Vector;
+
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
@@ -34,6 +39,7 @@ public class SleepPlugin extends JavaPlugin implements Listener {
 
         // Command handlers
         getCommand("sleepy").setExecutor(this);
+        
 
         PluginDescriptionFile pluginDescription = this.getDescription();
         getLogger().info("The " + pluginDescription.getName() + " version " + pluginDescription.getVersion() + " salutes you!");
@@ -43,28 +49,66 @@ public class SleepPlugin extends JavaPlugin implements Listener {
         Bukkit.broadcastMessage("SleepPlugin: " + message);
     }
 
+    private boolean votingPending = false;
+    private int positiveVotes = 0;
+    private int negativeVotes = 0;
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        shout("" + sender + " sent " + command.getName() + " " + String.join(" ", args));
+        shout("" + sender + " sent " + command.getName() + " " + String.join(" ", args));        
+        
+        if (args[0].equals("Yes") && !votingPending/* && positiveVotes + negativeVotes < getServer().getOnlinePlayers().size()*/) {
+            shout(sender.getName() + "voted for sleeping");
+            ++positiveVotes;
+            shout("Currently there are" + positiveVotes + "for sleeping, and" + negativeVotes + "against sleeping");
+        }
+        
+        if (args[0].equals("No") && !votingPending/* && positiveVotes + negativeVotes < getServer().getOnlinePlayers().size()*/) {
+            shout(sender.getName() + "voted against sleeping");
+            ++positiveVotes;
+            shout("Currently there are" + positiveVotes + "votes for sleeping, and" + negativeVotes + "against sleeping");
+        }
+
         return true;
     }
 
-    @EventHandler
-    public void onPlayerBedEnter(PlayerBedEnterEvent event) {
-        if (event.getBedEnterResult() != BedEnterResult.OK) return;
-
+    
+    
+    public boolean voting() {
         var msg = new ComponentBuilder("OK to sleep?  ")
             .append("[Yes]").color(ChatColor.DARK_GREEN).bold(true).event(new ClickEvent(Action.RUN_COMMAND, "/sleepy yes"))
             .append("  ")
             .append("[No]").color(ChatColor.DARK_RED).bold(true).event(new ClickEvent(Action.RUN_COMMAND, "/sleepy no"))
             .create();
-
         // Lehetne egyszerre mindenkinek kuldeni igy:
         // getServer().spigot().broadcast(msg);
+        votingPending = true;
+        
+        getServer().spigot().broadcast(msg);
+        
+        
+        
+        
+        votingPending = false;
+        return false;
+    }
+
+
+
+    @EventHandler
+    public void onPlayerBedEnter(PlayerBedEnterEvent event) {
+        if (votingPending) {
+            event.getPlayer().chat("/sleepy yes");
+            return;
+        }
+        
+        if (event.getBedEnterResult() != BedEnterResult.OK) return;
+
+        if (!voting()) return;
+        
 
         shout("" + event.getPlayer().getName() + " wants to sleep");
         for (var p : getServer().getOnlinePlayers()) {
-            p.spigot().sendMessage(msg);
             if (p == event.getPlayer()) continue;
 
             p.setGameMode(GameMode.SPECTATOR);
